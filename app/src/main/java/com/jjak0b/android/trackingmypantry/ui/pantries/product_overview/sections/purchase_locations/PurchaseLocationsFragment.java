fcd2e7359cb14262d09ebb3f843002deb1da0fae
jjak0b <jacopo.rimediotti@studio.unibo.it>
2021-08-27 19:18:27 +0200

    Add Google Maps MapView with API KEY related dependencies and plugins Update Gradle and gradle plugin

--- app/src/main/java/com/jjak0b/android/trackingmypantry/ui/pantries/product_overview/sections/purchase_locations/PurchaseLocationsFragment.java
+++ app/src/main/java/com/jjak0b/android/trackingmypantry/ui/pantries/product_overview/sections/purchase_locations/PurchaseLocationsFragment.java
@@ -12,6 +12,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.jjak0b.android.trackingmypantry.R;
 import com.jjak0b.android.trackingmypantry.ui.pantries.product_overview.ProductOverviewViewModel;
 
@@ -19,6 +26,7 @@ public class PurchaseLocationsFragment extends Fragment {
 
     private PurchaseLocationsViewModel mViewModel;
     private ProductOverviewViewModel mProductViewModel;
+    private OnMapReadyCallback onMapReadyCallback;
 
     public static PurchaseLocationsFragment newInstance() {
         return new PurchaseLocationsFragment();
@@ -41,6 +49,32 @@ public class PurchaseLocationsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+
+        onMapReadyCallback = new OnMapReadyCallback() {
+
+            /**
+             * Manipulates the map once available.
+             * This callback is triggered when the map is ready to be used.
+             * This is where we can add markers or lines, add listeners or move the camera.
+             * In this case, we just add a marker near Sydney, Australia.
+             * If Google Play services is not installed on the device, the user will be prompted to
+             * install it inside the SupportMapFragment. This method will only be triggered once the
+             * user has installed Google Play services and returned to the app.
+             */
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                LatLng sydney = new LatLng(-34, 151);
+                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            }
+        };
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(onMapReadyCallback);
+        }
     }
 
 }
