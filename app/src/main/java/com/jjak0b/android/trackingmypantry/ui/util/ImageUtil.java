a997d081dd7265c7cab9e1c10b9e9f3be9aacc5a
jjak0b <jacopo.rimediotti@studio.unibo.it>
2021-11-15 19:00:57 +0100

    Add utility methods in ImageUtils to convert bitmap and image uri using LiveData in async way

--- app/src/main/java/com/jjak0b/android/trackingmypantry/ui/util/ImageUtil.java
+++ app/src/main/java/com/jjak0b/android/trackingmypantry/ui/util/ImageUtil.java
@@ -12,11 +12,27 @@ import android.util.Base64;
 import androidx.activity.result.contract.ActivityResultContract;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.WorkerThread;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.android.gms.common.api.Api;
+import com.jjak0b.android.trackingmypantry.AppExecutors;
+import com.jjak0b.android.trackingmypantry.data.api.ApiResponse;
+import com.jjak0b.android.trackingmypantry.data.api.NetworkBoundResource;
+import com.jjak0b.android.trackingmypantry.data.api.Resource;
+import com.jjak0b.android.trackingmypantry.data.api.Transformations;
 
 import java.io.ByteArrayOutputStream;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import retrofit2.Response;
 
 public class ImageUtil
 {
+
+    @WorkerThread
     public static Bitmap convert(String base64Str) throws IllegalArgumentException
     {
         byte[] decodedBytes = Base64.decode(
@@ -27,6 +43,7 @@ public class ImageUtil
         return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
     }
 
+    @WorkerThread
     public static String convert(Bitmap bitmap)
     {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -34,6 +51,36 @@ public class ImageUtil
 
 
         return "data:image/jpeg;base64," + Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT);
+    }
+
+
+    public static LiveData<Resource<Bitmap>> getBitmap(String base64Str) {
+        AppExecutors appExecutors = AppExecutors.getInstance();
+        MutableLiveData<Resource<Bitmap>> mBitmap = new MutableLiveData<>(Resource.loading(null));
+        appExecutors.networkIO().execute(() -> {
+            try {
+                mBitmap.postValue(Resource.success(convert(base64Str)));
+            }
+            catch (Throwable e) {
+                mBitmap.postValue(Resource.error(e, null));
+            }
+        });
+        return mBitmap;
+    }
+
+
+    public static LiveData<Resource<String>> getURI(Bitmap bitmap) {
+        AppExecutors appExecutors = AppExecutors.getInstance();
+        MutableLiveData<Resource<String>> mURI = new MutableLiveData<>(Resource.loading(null));
+        appExecutors.networkIO().execute(() -> {
+            try {
+                mURI.postValue(Resource.success(convert(bitmap)));
+            }
+            catch (Throwable e) {
+                mURI.postValue(Resource.error(e, null));
+            }
+        });
+        return mURI;
     }
 
     public static class ActivityResultContractTakePicture extends ActivityResultContract<Void, Bitmap> {
