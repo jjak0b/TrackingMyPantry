b49779efcb8c88d363b19ec38f992fbe77a1f9d2
jjak0b <jacopo.rimediotti@studio.unibo.it>
2021-12-20 19:33:47 +0100

    Add error strings and ErrorsUtils

---
+++ app/src/main/java/com/jjak0b/android/trackingmypantry/ui/util/ErrorsUtils.java
@@ -0,0 +1,56 @@
+package com.jjak0b.android.trackingmypantry.ui.util;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.jjak0b.android.trackingmypantry.R;
+import com.jjak0b.android.trackingmypantry.data.api.AuthException;
+import com.jjak0b.android.trackingmypantry.data.api.NotLoggedInException;
+import com.jjak0b.android.trackingmypantry.data.api.RemoteException;
+
+import java.io.IOException;
+
+public class ErrorsUtils {
+
+    public static String getErrorMessage(final Context context, Throwable error, String debugTag) {
+        String errorMsg;
+
+        if( error instanceof NotLoggedInException ) {
+            Log.e( debugTag, "Authentication Error", error );
+            errorMsg = context.getString(R.string.error_msg_type_cause,
+                    context.getString(R.string.error_type_authentication),
+                    context.getString(R.string.error_cause_not_logged)
+            );
+        }
+        else if( error instanceof AuthException){
+            Log.e( debugTag, "Authentication Error", error );
+            errorMsg = context.getString(R.string.error_msg_type_cause,
+                    context.getString(R.string.error_type_authentication),
+                    context.getString(R.string.signIn_failed)
+            );
+        }
+        else if( error instanceof RemoteException){
+            Log.e( debugTag, "Server Error", error );
+            errorMsg = context.getString(R.string.error_msg_type_cause,
+                    context.getString(R.string.error_type_remote),
+                    context.getString(R.string.error_cause_bad_data)
+            );
+        }
+        else if( error instanceof IOException){
+            Log.e( debugTag, "Network Error", error );
+            errorMsg = context.getString(R.string.error_msg_type_cause,
+                    context.getString(R.string.error_type_network),
+                    context.getString(R.string.error_cause_unable_communicate)
+            );
+        }
+        else {
+            Log.e( debugTag, "Unexpected Error", error );
+            errorMsg = context.getString(R.string.error_msg_type_cause,
+                    context.getString(R.string.error_type_unhandled),
+                    context.getString(R.string.error_cause_unable_do_operation)
+            );
+        }
+
+        return errorMsg;
+    }
+}
