5e80c3dc73c6a947aa87af8d8b60c05e51895428
jjak0b <jacopo.rimediotti@studio.unibo.it>
2021-12-22 21:00:41 +0100

    Refactor ProductDetails - missed commit

--- app/src/main/java/com/jjak0b/android/trackingmypantry/ui/products/details/ProductDetailsFragment.java
+++ app/src/main/java/com/jjak0b/android/trackingmypantry/ui/products/details/ProductDetailsFragment.java
@@ -1,49 +1,20 @@
 package com.jjak0b.android.trackingmypantry.ui.products.details;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.DrawableRes;
-import androidx.lifecycle.ViewModelProvider;
-
-import android.Manifest;
-import android.content.ActivityNotFoundException;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
-import android.text.Editable;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.bumptech.glide.Glide;
 import com.hootsuite.nachos.NachoTextView;
 import com.hootsuite.nachos.terminator.ChipTerminatorHandler;
 import com.jjak0b.android.trackingmypantry.R;
 import com.jjak0b.android.trackingmypantry.data.db.entities.ProductTag;
 import com.jjak0b.android.trackingmypantry.ui.util.ChipTagUtil;
-import com.jjak0b.android.trackingmypantry.ui.util.ImageUtil;
-import com.jjak0b.android.trackingmypantry.ui.util.InputUtil;
-import com.jjak0b.android.trackingmypantry.ui.util.Permissions;
 
-public class ProductDetailsFragment extends Fragment {
-
-    protected ProductDetailsViewModel mViewModel;
-    protected ActivityResultLauncher<Void> takePictureLauncher;
-    protected ActivityResultLauncher<String[]> requestCameraPermissionsLauncher;
-    @DrawableRes
-    protected static final int RESOURCE_LOADING_PRODUCT_IMG = R.drawable.loading_spinner;
-    @DrawableRes
-    protected static final int RESOURCE_DEFAULT_PRODUCT_IMG = R.drawable.ic_baseline_product_placeholder;
-
+public class ProductDetailsFragment extends ProductInfoFragment {
 
     public static ProductDetailsFragment newInstance() {
         return new ProductDetailsFragment();
@@ -53,80 +24,15 @@ public class ProductDetailsFragment extends Fragment {
         return new ViewModelProvider(this).get(ProductDetailsViewModel.class);
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mViewModel = initViewModel();
-
-        takePictureLauncher = registerForActivityResult( new ImageUtil.ActivityResultContractTakePicture(), result -> {
-            if( result != null ){
-                mViewModel.setImage(result);
-            }
-        });
-
-        requestCameraPermissionsLauncher = registerForActivityResult( new ActivityResultContracts.RequestMultiplePermissions(), areGranted -> {
-            if( !areGranted.containsValue(false) ) {
-                takePicture();
-            }
-        });
-    }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_section_product_details, container, false);
+    private ProductDetailsViewModel getViewModel() {
+        return (ProductDetailsViewModel)mViewModel;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        final EditText editBarcode = (EditText) view.findViewById(R.id.editTextBarcode);
-        final EditText editName = (EditText) view.findViewById( R.id.editProductName );
-        final EditText editDescription = (EditText) view.findViewById( R.id.editProductDescription );
-        final View productForm = view.findViewById(R.id.productForm);
-        final ImageButton photoPreviewBtn = (ImageButton) view.findViewById(R.id.photoPreviewBtn);
-        final ImageView photoPreview = (ImageView) view.findViewById(R.id.photoPreview);
-        final View sectionTakePhoto = (View) view.findViewById(R.id.photoPreviewBtn);
         final NachoTextView chipsInput = (NachoTextView) view.findViewById(R.id.chips_input);
-
-        productForm.setVisibility(View.VISIBLE);
-        boolean hasFeatureCamera = getContext().getPackageManager()
-                .hasSystemFeature( PackageManager.FEATURE_CAMERA_ANY );
-
-        if( !hasFeatureCamera ){
-            sectionTakePhoto.setVisibility( View.GONE );
-        }
-        else {
-            photoPreviewBtn.setOnClickListener(v -> {
-                boolean hasPermissions = new Permissions.FeatureRequestBuilder()
-                        .setRationaleMessage(R.string.register_product_take_photo)
-                        .setOnPositive(requestCameraPermissionsLauncher, new String[]{Manifest.permission.CAMERA} )
-                        .show(requireContext());
-                if( hasPermissions ){
-                    takePicture();
-                }
-            });
-        }
-
-        editBarcode.addTextChangedListener( new InputUtil.FieldTextWatcher() {
-            @Override
-            public void afterTextChanged(Editable s) {
-                mViewModel.setBarcode(s.toString());
-            }
-        });
-        editName.addTextChangedListener( new InputUtil.FieldTextWatcher() {
-            @Override
-            public void afterTextChanged(Editable s) {
-                mViewModel.setName(s.toString());
-            }
-        });
-        editDescription.addTextChangedListener( new InputUtil.FieldTextWatcher() {
-            @Override
-            public void afterTextChanged(Editable s) {
-                mViewModel.setDescription(s.toString());
-            }
-        });
 
         ArrayAdapter<ProductTag> adapter = new ArrayAdapter<>(getContext(),
                 android.R.layout.simple_spinner_dropdown_item);
@@ -134,45 +40,21 @@ public class ProductDetailsFragment extends Fragment {
         chipsInput.addChipTerminator('\n', ChipTerminatorHandler.BEHAVIOR_CHIPIFY_TO_TERMINATOR );
         chipsInput.setOnFocusChangeListener( (v, hasFocus) -> {
             if( !hasFocus ){
-                mViewModel.setAssignedTags( ChipTagUtil.newTagsInstanceFromChips( chipsInput.getAllChips() ) );
+                getViewModel().setAssignedTags( ChipTagUtil.newTagsInstanceFromChips( chipsInput.getAllChips() ) );
             }
         });
 
-        mViewModel.getBarcode().observe(getViewLifecycleOwner(), s -> {
-            editBarcode.setText(s);
-            editBarcode.setSelection(s.length());
-        });
-
-        mViewModel.getName().observe(getViewLifecycleOwner(), s -> {
-            editName.setText(s);
-            editName.setSelection(s.length());
-        });
-        mViewModel.getDescription().observe(getViewLifecycleOwner(), s -> {
-            editDescription.setText(s);
-            editDescription.setSelection(s.length());
-        });
-        mViewModel.getImage().observe( getViewLifecycleOwner(), bitmap -> {
-            Glide.with(view)
-                    .load(bitmap)
-                    .fitCenter()
-                    .placeholder(RESOURCE_LOADING_PRODUCT_IMG)
-                    .fallback(RESOURCE_DEFAULT_PRODUCT_IMG)
-                    .into(photoPreview);
-        });
-        mViewModel.getAssignedTags().observe( getViewLifecycleOwner(), productTags -> {
+        getViewModel().getAssignedTags().observe( getViewLifecycleOwner(), productTags -> {
             chipsInput.setTextWithChips( ChipTagUtil.newChipsInstanceFromTags( productTags ) );
             chipsInput.setSelection(chipsInput.getText().length());
         });
-        mViewModel.getSuggestionTags().observe( getViewLifecycleOwner(), productTags -> {
+        getViewModel().getSuggestionTags().observe( getViewLifecycleOwner(), productTags -> {
             adapter.clear();
             adapter.addAll( productTags );
         });
 
-        mViewModel.onSave().observe( getViewLifecycleOwner(), shouldSave -> {
+        getViewModel().onSave().observe( getViewLifecycleOwner(), shouldSave -> {
             if( !shouldSave ) return;
-
-            // close any open keyboard
-            InputUtil.hideKeyboard(requireActivity());
 
             // setOnFocusChangeListener of chips tags view is not triggered while clicking on a "save" view
             // so trigger it manually
@@ -181,14 +63,5 @@ public class ProductDetailsFragment extends Fragment {
                 // mViewModel.setAssignedTags(ChipTagUtil.newTagsInstanceFromChips(chipsInput.getAllChips()));
             }
         });
-    }
-
-    private void takePicture() {
-        try {
-            takePictureLauncher.launch(null);
-        }
-        catch (ActivityNotFoundException e) {
-            Toast.makeText(getContext(), R.string.error_unable_to_take_picture, Toast.LENGTH_LONG ).show();
-        }
     }
 }
--- app/src/main/java/com/jjak0b/android/trackingmypantry/ui/products/details/ProductDetailsViewModel.java
+++ app/src/main/java/com/jjak0b/android/trackingmypantry/ui/products/details/ProductDetailsViewModel.java
@@ -1,106 +1,89 @@
 package com.jjak0b.android.trackingmypantry.ui.products.details;
 
 import android.app.Application;
-import android.graphics.Bitmap;
 
+import androidx.annotation.NonNull;
 import androidx.arch.core.util.Function;
-import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Transformations;
 
 import com.hadilq.liveevent.LiveEvent;
-import com.jjak0b.android.trackingmypantry.data.repositories.PantryRepository;
+import com.jjak0b.android.trackingmypantry.data.api.Resource;
 import com.jjak0b.android.trackingmypantry.data.db.entities.ProductTag;
 import com.jjak0b.android.trackingmypantry.data.db.relationships.ProductWithTags;
-import com.jjak0b.android.trackingmypantry.ui.util.ImageUtil;
+import com.jjak0b.android.trackingmypantry.data.repositories.PantryRepository;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
-public class ProductDetailsViewModel extends AndroidViewModel {
+public class ProductDetailsViewModel extends ProductInfoViewModel {
 
     protected PantryRepository pantryRepository;
-    private MutableLiveData<ProductWithTags> originalProduct;
-    private MutableLiveData<String> barcode;
-    private MutableLiveData<String> name;
-    private MutableLiveData<String> description;
+    private MutableLiveData<List<ProductTag>> original;
     private MutableLiveData<List<ProductTag>> assignedTags;
-    private MutableLiveData<Bitmap> image;
-    private LiveEvent<Boolean> onSave;
+    private LiveEvent<Resource<List<ProductTag>>> mTagsResult;
 
-    public ProductDetailsViewModel(Application application) {
+    public ProductDetailsViewModel(@NonNull Application application) {
         super(application);
 
         pantryRepository = PantryRepository.getInstance(application);
-        originalProduct = new MutableLiveData<>();
-
-        barcode = (MutableLiveData<String>) Transformations.map(originalProduct,
-                input -> input != null ? input.product.getBarcode() : null
-        );
-
-        name = (MutableLiveData<String>) Transformations.map(originalProduct,
-                input -> input != null ? input.product.getName() : null
-        );
-
-        description = (MutableLiveData<String>) Transformations.map(originalProduct,
-                input -> input != null ? input.product.getDescription() : null
-        );
-
-        image = (MutableLiveData<Bitmap>) Transformations.map(
-                originalProduct,
-                input -> {
-                    if( input != null && input.product.getImg() != null ) {
-                        try {
-                            return ImageUtil.convert(input.product.getImg());
-                        } catch (IllegalArgumentException exception) {
-                            return null;
-                        }
-                    }
-                    return null;
-                }
-        );
 
         assignedTags = (MutableLiveData<List<ProductTag>>) Transformations.map(
-                originalProduct,
-                (Function<ProductWithTags, List<ProductTag>>) input -> {
+                original,
+                (Function<List<ProductTag>, List<ProductTag>>) input -> {
                     // maybe should be deep copy
                     if( input != null )
-                        return new ArrayList<>(input.tags);
+                        return new ArrayList<>(input);
                     else
                         return new ArrayList<>(0);
                 }
         );
-
-        onSave = new LiveEvent<>();
     }
 
     @Override
-    protected void onCleared() {
-        originalProduct.setValue(null);
-        originalProduct = null;
-        super.onCleared();
-    }
-
     public void save() {
-        onSave.setValue(true);
-        onSave.postValue(false);
+        super.save();
+        // tags stuff
+
+        ProductWithTags value = new ProductWithTags();
+        mTagsResult.addSource(onSave(), aBoolean -> {
+            if( aBoolean ){
+                mTagsResult.setValue(Resource.loading(null));
+                return;
+            }
+            mTagsResult.removeSource(onSave());
+
+            mTagsResult.addSource(getAssignedTags(), productTags -> {
+                mTagsResult.removeSource(getAssignedTags());
+                mTagsResult.setValue(Resource.success(productTags));
+            });
+        });
     }
 
-    public LiveData<Boolean> onSave() {
-        return onSave;
-    }
-
-    public LiveData<ProductWithTags> getProduct() {
-        return originalProduct;
+    public LiveData<Resource<ProductWithTags>> getResultProductWithTags() {
+        return com.jjak0b.android.trackingmypantry.data.api.Transformations.forward(super.onSaved(), productResource -> {
+            return com.jjak0b.android.trackingmypantry.data.api.Transformations.forward(mTagsResult, tagsResource -> {
+                ProductWithTags productWithTags = new ProductWithTags();
+                productWithTags.product = productResource.getData();
+                productWithTags.tags = tagsResource.getData();
+                return new MutableLiveData<>(Resource.success(productWithTags));
+            });
+        });
     }
 
     public void setProduct(ProductWithTags productWithTags) {
-        originalProduct.setValue(productWithTags);
+        if( productWithTags != null){
+            super.setProduct(productWithTags.product);
+            original.setValue(productWithTags.tags);
+        }
+        else {
+            super.setProduct(null);
+            original.setValue(null);
+        }
     }
 
-    public void setAssignedTags( List<ProductTag> tags ) {
+    public void setAssignedTags(List<ProductTag> tags ) {
         assignedTags.setValue( tags );
     }
 
@@ -111,41 +94,4 @@ public class ProductDetailsViewModel extends AndroidViewModel {
     public LiveData<List<ProductTag>> getSuggestionTags() {
         return pantryRepository.getAllProductTags();
     }
-
-    public LiveData<Bitmap> getImage() {
-        return image;
-    }
-
-    public void setImage(Bitmap image) {
-        if( !Objects.equals(this.image.getValue(), image) )
-            this.image.setValue(image);
-    }
-
-    public LiveData<String> getBarcode() {
-        return barcode;
-    }
-
-    public void setBarcode(String barcode){
-        if( !Objects.equals(this.barcode.getValue(), barcode) )
-            this.barcode.setValue(barcode);
-    }
-
-    public LiveData<String> getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        if( !Objects.equals(this.name.getValue(), name) )
-            this.name.setValue(name);
-    }
-
-    public LiveData<String> getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        if( !Objects.equals(this.description.getValue(), description) )
-            this.description.setValue(description);
-    }
-
 }
