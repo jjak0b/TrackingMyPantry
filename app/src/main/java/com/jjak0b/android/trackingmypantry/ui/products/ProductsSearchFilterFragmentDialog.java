070b18f0d459b3d9704bb876a66dd4aac9dd7a5b
jjak0b <jacopo.rimediotti@studio.unibo.it>
2022-02-08 12:23:48 +0100

    Add no tags available message for tags filter dialog

--- app/src/main/java/com/jjak0b/android/trackingmypantry/ui/products/ProductsSearchFilterFragmentDialog.java
+++ app/src/main/java/com/jjak0b/android/trackingmypantry/ui/products/ProductsSearchFilterFragmentDialog.java
@@ -2,12 +2,15 @@ package com.jjak0b.android.trackingmypantry.ui.products;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.jjak0b.android.trackingmypantry.R;
@@ -15,10 +18,14 @@ import com.jjak0b.android.trackingmypantry.data.db.entities.ProductTag;
 import com.jjak0b.android.trackingmypantry.ui.util.MultiSelectItemsFragmentDialog;
 import com.jjak0b.android.trackingmypantry.ui.util.MultiSelectItemsViewModel;
 
+import java.util.List;
+
 public class ProductsSearchFilterFragmentDialog extends MultiSelectItemsFragmentDialog<ProductTag> {
 
     private ProductsSearchFilterViewModel mSearchViewModel;
     private MultiSelectProductsTagsViewModel mViewModel;
+    private AlertDialog dialog;
+    private static final String HEADER_TAG = "header";
 
     public static ProductsSearchFilterFragmentDialog newInstance() {
         return new ProductsSearchFilterFragmentDialog();
@@ -43,6 +50,17 @@ public class ProductsSearchFilterFragmentDialog extends MultiSelectItemsFragment
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         Dialog dialog = super.onCreateDialog(savedInstanceState);
         dialog.setTitle(R.string.setup_filter_options);
+        this.dialog = (AlertDialog) dialog;
+
+        TextView header = new TextView(requireContext());
+        header.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        );
+        header.setGravity(Gravity.CENTER);
+        header.setId(View.generateViewId());
+        header.setTag(HEADER_TAG);
+
+        this.dialog.getListView().addHeaderView(header, HEADER_TAG, false);
         return dialog;
     }
 
@@ -55,6 +73,31 @@ public class ProductsSearchFilterFragmentDialog extends MultiSelectItemsFragment
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        TextView header = getHeaderView();
+
+        mViewModel.getAllItems().observe(getViewLifecycleOwner(), resource -> {
+            switch (resource.getStatus()) {
+                case LOADING:
+                    header.setText(R.string.loading);
+                    header.setVisibility(View.VISIBLE);
+                    break;
+                default:
+                    List<ProductTag> items = resource.getData();
+
+                    if( header != null ) {
+                        if( items != null && items.isEmpty() ) {
+                            header.setText(R.string.filter_no_options_available);
+                            header.setVisibility(View.VISIBLE);
+                        }
+                        else {
+                            header.setText(null);
+                            header.setVisibility(View.GONE);
+                        }
+                    }
+                    break;
+            }
+        });
 
         mSearchViewModel.getSearchTags().observe(getViewLifecycleOwner(), tags -> {
             mViewModel.setItems(tags);
@@ -71,5 +114,10 @@ public class ProductsSearchFilterFragmentDialog extends MultiSelectItemsFragment
                     break;
             }
         });
+    }
+
+
+    private TextView getHeaderView() {
+        return this.dialog.getListView().findViewWithTag(HEADER_TAG);
     }
 }
